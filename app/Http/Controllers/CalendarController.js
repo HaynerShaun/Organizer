46f0158e40b726f1c90f3cd333acbbc3a0d6f0d1
HaynerShaun <shaunhayner@Shauns-MacBook-Pro.local>
2017-04-04 09:26:50 -0400

    Created anticipated views and controllers

---
+++ app/Http/Controllers/CalendarController.js
@@ -0,0 +1,7 @@
+'use strict'
+
+class CalendarController {
+
+}
+
+module.exports = CalendarController
---
+++ app/Http/Controllers/MealController.js
@@ -0,0 +1,7 @@
+'use strict'
+
+class MealController {
+
+}
+
+module.exports = MealController
